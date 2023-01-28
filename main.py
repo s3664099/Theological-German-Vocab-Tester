c4f48a07729cd757b67bed0408294e1b631ed366
Dave Sarkies <s3664099@student.rmit.edu.au>
2023-01-28 13:59:52 +1100

    Added code so that multiple answers can be given

--- main.py
+++ main.py
@@ -15,11 +15,11 @@ app = Flask(__name__,
 	static_folder = 'static')
 app.secretkey = os.environ.get("FLASK_API_KEY")
 
+#Generates the main screen
 @app.route('/')
 def main():
 
+	#Picks a random work from the vocab list.
 	german,english = random.choice(list(vocab.vocabulary.items()))
-
-	print(english)
 
 	return render_template("index.html",german=german,english=english)
--- static/script.js
+++ static/script.js
@@ -3,37 +3,81 @@ File: Theological German Vocab Tester
 Author: David Sarkies
 Initial: 22 January 2023
 Update: 22 January 2023
-Version: 0.1
+Version: 0.2
 */
 
+//Checks if the answer is correct
 function checkAnswer() {
+
+	//Gets the answers and the attempts
 	answer = document.getElementById("eng");
 	answers = answer.value.split(",");
 	attempt = document.getElementById("input");
+	attempts = attempt.value.split(",")
 	attempt.readonly = true;
 
-	var correct = false;
+	//Checks how correct the answer is
+	var correct = compareAnswers(answers,attempts);
 
-	for (var x=0;x<answers.length; x++) {
-
-		if (attempt.value == answers[x].trim()) {
-			correct = true;
-		}
-	}
-
-	if (correct) {
+	//Sets the input as green of all the words guessed
+	if (correct == 2) {
 		attempt.style.backgroundColor = "LightGreen";
 		createNextButton(attempt);
+
+	//sets it as yellow if only a few
+	} else if (correct == 1) {
+		attempt.style.backgroundColor = "yellow";
+
+	//Sets it red if it was wrong
 	} else {
 		attempt.style.backgroundColor = "pink";
 	}
 }
 
+//Function that goes through the answers and the attempts, and checks which are correct
+function compareAnswers(answers,attempts) {
+
+	//Sets the correct value, and the number correct to 0.
+	var correct = 0;
+	var numberCorrect = 0;
+
+	//Goes through each of the answers
+	for (var x=0;x<answers.length; x++) {
+
+		correctNumber = -1;
+
+		//Goes through each of the attempts
+		for (var y=0;y<attempts.length;y++) {
+
+			//If an attempt is correct, a correct is marked
+			if (attempts[y].trim() == answers[x].trim()) {
+				numberCorrect ++;
+				correctNumber = y;
+				correct = 1;
+			}
+		}
+
+		//The correct answer is removed from the attempt array
+		if (correctNumber != -1) {
+			attempts.splice(correctNumber,1);
+		}
+	}
+
+	//If all are correct, the number is set
+	if (numberCorrect == answers.length) {
+		correct = 2;
+	}
+
+	return correct;
+}
+
+//Refreshes the page
 function reloadPage() {
 	attempt.value = "";
 	window.location.reload();
 }
 
+//Displays the correct answer
 function showAnswer() {
 	attempt = document.getElementById("input");
 	const answer = document.getElementById("giveAnswer");
@@ -41,6 +85,7 @@ function showAnswer() {
 	createNextButton(attempt);
 }
 
+//Creates a button to get the next question
 function createNextButton() {
 	const newButt = document.getElementById("reloadButton");
 	const newButton = document.createElement("button");
